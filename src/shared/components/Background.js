082401a997b0e946cd117cbf1e53601f369d9fc5
Bill Dybas <wmdybas@gmail.com>
2018-03-02 22:45:36 -0500

    Add Flexbox to Background
    - Enables the Archive Page fixes

--- src/shared/components/Background.js
+++ src/shared/components/Background.js
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 
 export default styled.div`
   background-color: ${props => props.color};
+  display: flex;
+  flex-direction: column;
   min-height: 100vh;
   width: 100%;
 `
