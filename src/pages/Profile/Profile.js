5ccaeb158476336ed8cb34d6e2255fb6606ea7cc
Bill Dybas <wmdybas@gmail.com>
2021-09-20 07:08:51 -0700

    Pass Linting to Build

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,9 +1,7 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
+import React from "react";
+// import { Link } from 'react-router-dom'
+// import styled from 'styled-components'
 
-const Profile = () => (
-  <div></div>
-)
+const Profile = () => <div></div>;
 
-export default Profile
+export default Profile;
